b2b1c29ae6d8cbfcf6cb647dcf34028925460edf
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-10 13:22:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+# method takes in an integer argument 
+# use while loop to countdown to 0 (-=)
+# puts "#{number} SECOND (S) !"
+# return "HAPPY NEW YEAR!"
 
-def countdown
+def countdown(integer)
+  while integer > 0 
+    puts "#{integer} SECOND (S)!"
+    integer -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
